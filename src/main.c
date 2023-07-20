0f365c9c679c2e853ed11e474d973232bf237c45
JanotLeLapin <joseph300905@gmail.com>
2023-07-20 06:13:42 +0200

    Basic libcurl & json-c usage

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <notcurses/direct.h>
 #include <notcurses/notcurses.h>
+#include <curl/curl.h>
+#include <json-c/json_tokener.h>
+#include <json-c/json_object.h>
 
 void middle_print(struct ncplane *n, char *text) {
   unsigned int y;
@@ -9,6 +13,22 @@ void middle_print(struct ncplane *n, char *text) {
   ncplane_dim_yx(n, &y, &x);
 
   ncplane_printf_yx(n, y / 2, (x / 2) - (strlen(text) / 2), text);
+}
+
+static size_t callback(void *contents, size_t size, size_t nmemb, void *data) {
+  struct ncplane *n = (struct ncplane *) data;
+
+  char* txt = malloc(64);
+  strcpy(txt, "Welcome: ");
+  struct json_object *obj = json_tokener_parse(contents);
+  for (int i = 0; i < json_object_array_length(obj); i++) {
+    json_object *elem = json_object_array_get_idx(obj, i);
+    strcat(txt, json_object_get_string(elem));
+    strcat(txt, " ");
+  }
+
+  middle_print(n, txt);
+  return size * nmemb;
 }
 
 int main() {
@@ -19,20 +39,38 @@ int main() {
     return -1;
   }
 
+  CURL *curl = curl_easy_init();
+  if (!curl) {
+    printf("Whoops\n");
+    return -1;
+  }
+
   notcurses_enter_alternate_screen(nc);
 
-
   struct ncplane *n = notcurses_stdplane(nc);
-  middle_print(n, "Welcome to Chord!");
+  middle_print(n, "Loading...");
+  ncpile_render(n);
+  notcurses_render(nc);
+
+  curl_easy_setopt(curl, CURLOPT_URL, "https://random-word-api.herokuapp.com/word?number=3");
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, n);
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+
+  CURLcode res = curl_easy_perform(curl);
+
+  if (res != CURLE_OK) {
+    printf("Whoops\n");
+  }
 
   while (true) {
     ncpile_render(n);
-    // ncpile_rasterize(n);
     notcurses_render(nc);
   }
 
   notcurses_leave_alternate_screen(nc);
   notcurses_stop(nc);
+
+  curl_easy_cleanup(curl);
 
   return 0;
 }
